bfffbe946dafc13c20307afe85ecda9e24683f83
surya1701 <suryavardan17@gmail.com>
2020-08-30 14:20:08 +0530

    Update tests.py

--- user/tests.py
+++ user/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
 
 # Create your tests here.
+from .models import Prediction
+
+
+class TestModel(TestCase):
+    def test_page_is_created_successfully(self):
+        pred = Prediction(
+            uname="Testf",
+            gender="Testl",
+            married="Yes",
+            depend="2",
+            education="Graduate",
+            self_employ="Yes",
+            aincome=100,
+            caincome=100,
+            loanamt=10,
+            loanterm=10,
+            credhist=1,
+            area="Rural",
+            pred="Y"
+        )
+        pred.save()
