d281fdc5a8266ae255c0266b2573a06cc368c4e8
surya1701 <suryavardan17@gmail.com>
2020-08-30 13:36:45 +0530

    Added Pytest

--- LoanApplication/loan/settings.py
+++ LoanApplication/loan/settings.py
@@ -83,6 +83,17 @@ DATABASES = {
         'HOST': 'localhost'
     }
 }
+if os.environ.get('GITHUB_WORKFLOW'):
+    DATABASES = {
+        'default': {
+           'ENGINE': 'django.db.backends.postgresql',
+           'NAME': 'github_actions',
+           'USER': 'postgres',
+           'PASSWORD': 'postgres',
+           'HOST': '127.0.0.1',
+           'PORT': '5432',
+        }
+    }
 
 
 # Password validation
